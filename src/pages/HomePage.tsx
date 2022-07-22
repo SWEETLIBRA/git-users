020a48764b2472440310131808fd9ae3498295e2
Igor <igor,zhuiko@mail.ru>
2022-07-22 17:21:10 +0300

    added params for custom hook

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useSerchUsersQuery } from "../store/github/github.api";
 
 export function HomePage() {
+    const {isLoading, isError, data} = useSerchUsersQuery('aigor')
     return(
         <div>home</div>
     )
