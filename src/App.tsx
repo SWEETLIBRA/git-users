50518bc6c120f80ba03abac47cc814934923de7b
Igor <igor,zhuiko@mail.ru>
2022-07-22 16:23:59 +0300

    create navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Navigation } from './components/Navigation';
+import { FavouritesPage } from './pages/FavouritesPage';
+import { HomePage } from './pages/HomePage';
 
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/favourites' element={<FavouritesPage />} />
+      </Routes>
+    </>
   );
 }
 
