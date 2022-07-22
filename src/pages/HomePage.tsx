31b5c49edb0023b4f476aee1ba62dd4da6316ca0
Igor <igor,zhuiko@mail.ru>
2022-07-22 18:06:58 +0300

    created state

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSerchUsersQuery } from "../store/github/github.api";
 
 export function HomePage() {
+    const [search, setSearch ] = useState('')
     const {isLoading, isError, data} = useSerchUsersQuery('aigor')
+
+    useEffect(() => {
+        console.log(search);
+    }, [search])
     
     return(
         <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
@@ -11,6 +16,8 @@ export function HomePage() {
             <div className="relative w-[560px]">
                 <input type="text" className="border py-2 px-4 w-full h-[42px] mb-2"
                     placeholder="Search for Github username..."
+                    value={search}
+                    onChange={e => setSearch(e.target.value)}
                 />
 
                 <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
