fd2441a25ca386bce717e7a16808bb30ffa8cc8a
Igor <igor,zhuiko@mail.ru>
2022-07-22 16:48:36 +0300

    create api

---
+++ src/store/github/github.api.ts
@@ -0,0 +1,16 @@
+import { build } from '@reduxjs/toolkit/dist/query/core/buildMiddleware/cacheLifecycle'
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+export const githubApi = createApi({
+    reducerPath: 'github/api',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://api.github.com/'
+    }),
+    endpoints: build => ({
+        serchUsers: build.query({
+            query: () =>({
+                url: `search/users`
+            })
+        })
+    })
+})
