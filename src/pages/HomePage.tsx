4619ab4512a2a29e0f736530affa9cc10dc9fcf5
Igor <igor,zhuiko@mail.ru>
2022-07-23 15:15:22 +0300

    dropdown clear

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,14 +4,15 @@ import { useSerchUsersQuery } from "../store/github/github.api";
 
 export function HomePage() {
     const [search, setSearch ] = useState('')
+    const [dropdown, setDropdown] = useState(false)
     const debounced = useDebounce(search)
     const {isLoading, isError, data} = useSerchUsersQuery(debounced, {
         skip: debounced.length < 3
     })
 
     useEffect(() => {
-        console.log(debounced);
-    }, [debounced])
+        setDropdown(debounced.length > 3 && data?.length! > 0 )
+    }, [debounced, data])
     
     return(
         <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
@@ -24,14 +25,14 @@ export function HomePage() {
                     onChange={e => setSearch(e.target.value)}
                 />
 
-                <ul className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white">
+                { dropdown && <ul className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white">
                     {isLoading && <p className="text-center">Loading...</p>}
                     { data?.map(user =>(
                         <li key={user.id}
                         className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
                         >{ user.login }</li>
                     )) }
-                </ul>
+                </ul>}
             </div>
         </div>
     )
