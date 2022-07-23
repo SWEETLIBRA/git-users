e068420974c66bf7f782b50a5685174c4b740d83
Igor <igor,zhuiko@mail.ru>
2022-07-23 17:34:05 +0300

    created addFavorite

--- src/store/github/github.slice.ts
+++ src/store/github/github.slice.ts
@@ -1,5 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
+const LS_FAV_KEY = "fav-key"
+
 interface GithubState {
     favourites: string[]
 }
@@ -12,10 +14,11 @@ export const githubSlice = createSlice({
     name: 'github',
     initialState,
     reducers: {
-        addFavorite(state, payload: PayloadAction) {
-
+        addFavorite(state, action: PayloadAction<string>) {
+            state.favourites.push(action.payload)
+            localStorage.setItem(LS_FAV_KEY, JSON.stringify(state.favourites))
         },
-        removeFavorite(state, payload: PayloadAction) {
+        removeFavorite(state, action: PayloadAction<string>) {
 
         },
     }
